16ed42857b035eb400f8a9f26f5962aedca2b5e7
reyesisamar455-svg <reyesisamar455@gmail.com>
2026-04-08 13:20:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let memoria = JSON.parse(localStorage.getItem("memoria")) || {};
 
+function responder(msg) {
+
+    msg = msg.toLowerCase();
+
+    if (memoria[msg]) return memoria[msg];
+
+    if (msg.includes("hola")) return "Hola 😈 soy Lisluz";
+
+    if (msg.includes("kit")) return "Usa /kit minero";
+
+    return "No sé 😅 enséñame";
+}
+
+function enviar() {
+
+    let input = document.getElementById("input");
+    let chat = document.getElementById("chat");
+
+    let msg = input.value;
+
+    chat.innerHTML += "<p>👤: " + msg + "</p>";
+
+    let res = responder(msg);
+
+    chat.innerHTML += "<p>🤖: " + res + "</p>";
+
+    if (res === "No sé 😅 enséñame") {
+        let nueva = prompt("¿Qué debo responder?");
+        if (nueva) {
+            memoria[msg.toLowerCase()] = nueva;
+            localStorage.setItem("memoria", JSON.stringify(memoria));
+        }
+    }
+
+    input.value = "";
+}
